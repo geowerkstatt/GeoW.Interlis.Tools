d61a1db2ee574f1ae11a9961c1bd954f17c0a79b
danjov <daniel.jovanovic@geowerkstatt.ch>
2024-06-11 09:49:43 +0200

    Remove unnecessary initializers

--- src/Geowerkstatt.Interlis.Compiler/AST/ClassDef.cs
+++ src/Geowerkstatt.Interlis.Compiler/AST/ClassDef.cs
@@ -3,12 +3,12 @@
 public sealed class ClassDef : IAstElement, IInterlisDefinition, IDocumentation, IContainer<IInterlisDefinition>
 {
     public required string Name { get; init; }
-    public IInterlisDefinition? Parent { get; set; } = null;
+    public IInterlisDefinition? Parent { get; set; }
 
     public IList<string> DocComments { get; } = new List<string>();
     public IDictionary<string, string> MetaAttributes { get; } = new Dictionary<string, string>();
 
-    public ClassDef? Extends { get; set; } = null;
+    public ClassDef? Extends { get; set; }
 
     public Dictionary<string, IInterlisDefinition> Content { get; } = new Dictionary<string, IInterlisDefinition>();
 
