f75144b27fb72d267236e25c999e11c9a5171774
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2025-06-16 09:29:56 +0200

    Remove commented out code

--- src/Geowerkstatt.Interlis.RepositoryCrawler/RepositoryCrawlerContext.cs
+++ src/Geowerkstatt.Interlis.RepositoryCrawler/RepositoryCrawlerContext.cs
@@ -13,18 +13,8 @@ internal class RepositoryCrawlerContext : DbContext
     public DbSet<Catalog> Catalogs { get; set; }
     public DbSet<InterlisFile> InterlisFiles { get; set; }
 
-    //private static string dbDirectory = Path.Combine(Path.GetTempPath(), "Geowerkstatt.Interlis");
-    //private static string dbFileName = $"ModelRepositoryCache{Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? ""}.db";
-    //public string DbPath { get; } = Path.Combine(dbDirectory, dbFileName);
-
     public RepositoryCrawlerContext(DbContextOptions<RepositoryCrawlerContext> options)
         : base(options)
     {
     }
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    Directory.CreateDirectory(dbDirectory);
-    //    optionsBuilder.UseSqlite($"Data Source={DbPath}");
-    //}
 }
