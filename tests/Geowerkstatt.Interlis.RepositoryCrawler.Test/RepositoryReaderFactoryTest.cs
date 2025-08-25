acb5f3a7a6b3773235fd8f056f2e196d6a5d01b8
Joel Wälti <joel.waelti@geowerkstatt.ch>
2025-08-25 17:07:19 +0200

    Add test cases for RepositoryReaderFactory

---
+++ tests/Geowerkstatt.Interlis.RepositoryCrawler.Test/RepositoryReaderFactoryTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Geowerkstatt.Interlis.RepositoryCrawler
+{
+    [TestClass]
+    public class RepositoryReaderFactoryTest
+    {
+        private void AssertReaderTypeForPathVariants(string basePath, Type expectedType)
+        {
+            var reader = RepositoryReaderFactory.Create(basePath);
+            var readerTrailing = RepositoryReaderFactory.Create(
+                basePath.EndsWith("/") || basePath.EndsWith("\\") ? basePath : basePath + (basePath.Contains("/") ? "/" : "\\")
+            );
+
+            Assert.IsInstanceOfType(reader, expectedType);
+            Assert.IsInstanceOfType(readerTrailing, expectedType);
+        }
+
+        [TestMethod]
+        public void CreateLocalRepositoryReaders()
+        {
+            AssertReaderTypeForPathVariants("C:\\test\\repository\\path", typeof(LocalRepositoryReader)); // Windows absolute path
+            AssertReaderTypeForPathVariants(".\\test\\repository\\path", typeof(LocalRepositoryReader)); // Windows relative path
+            AssertReaderTypeForPathVariants("\\\\test\\repository\\path", typeof(LocalRepositoryReader)); // Windows UNC path
+            AssertReaderTypeForPathVariants("/test/repository/path", typeof(LocalRepositoryReader)); // Unix absolute path
+            AssertReaderTypeForPathVariants("test/repository/path", typeof(LocalRepositoryReader)); // Unix relative path
+        }
+
+        [TestMethod]
+        public void CreateHttpRepositoryReaders()
+        {
+            AssertReaderTypeForPathVariants("http://example.com/repository/path", typeof(HttpRepositoryReader));
+            AssertReaderTypeForPathVariants("https://example.com/repository/path", typeof(HttpRepositoryReader));
+        }
+
+        [TestMethod]
+        public void CreateWithInvalidLocation()
+        {
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create(""));
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create("invalid path"));
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create("://invalid/repository/path"));
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create("./invalid/character>"));
+        }
+
+        [TestMethod]
+        public void CreateWithUnsupportedLocation()
+        {
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create("scheme://example.com/repository/path")); // Valid URI but unsupported scheme
+            Assert.ThrowsException<RepositoryReaderException>(() => RepositoryReaderFactory.Create("ftp://example.com/repository/path")); // FTP not supported (yet)
+        }
+    }
+}
